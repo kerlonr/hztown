4725bf7e84c01cf0c97ef20788513ee3b2ead01c
kerlonr <mitokerlon1227@gmail.com>
2026-05-27 15:26:35 -0300

    Configure server for Render deploy

--- server.js
+++ server.js
@@ -109,6 +109,6 @@ io.on("connection", (socket) => {
   });
 });
 
-server.listen(PORT, () => {
-  console.log(`Projeto GT rodando em http://localhost:${PORT}`);
+server.listen(PORT, "0.0.0.0", () => {
+  console.log(`Projeto GT rodando na porta ${PORT}`);
 });
